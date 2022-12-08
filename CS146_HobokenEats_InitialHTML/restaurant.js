5c2d1e9309f605af058785cf337baf0cb1a0722b
khalton55 <kyle@netrealm.com>
2022-12-08 11:28:27 -0500

    Added user comments

---
+++ CS146_HobokenEats_InitialHTML/restaurant.js
@@ -0,0 +1,48 @@
+function submitAyame(){
+    var list = document.getElementById("comments1")
+    var comment = document.getElementById("newComment1").value
+    var entry = document.createElement('li')
+    entry.appendChild(document.createTextNode(comment))
+    list.appendChild(entry)
+
+}
+function submitCharritos(){
+    var list = document.getElementById("comments2")
+    var comment = document.getElementById("newComment2").value
+    var entry = document.createElement('li')
+    entry.appendChild(document.createTextNode(comment))
+    list.appendChild(entry)
+
+}
+function submitGrimaldis(){
+    var list = document.getElementById("comments3")
+    var comment = document.getElementById("newComment3").value
+    var entry = document.createElement('li')
+    entry.appendChild(document.createTextNode(comment))
+    list.appendChild(entry)
+
+}
+function submitKarma(){
+    var list = document.getElementById("comments4")
+    var comment = document.getElementById("newComment4").value
+    var entry = document.createElement('li')
+    entry.appendChild(document.createTextNode(comment))
+    list.appendChild(entry)
+
+}
+function submitShake(){
+    var list = document.getElementById("comments5")
+    var comment = document.getElementById("newComment5").value
+    var entry = document.createElement('li')
+    entry.appendChild(document.createTextNode(comment))
+    list.appendChild(entry)
+
+}
+function submitObagel(){
+    var list = document.getElementById("comments6")
+    var comment = document.getElementById("newComment6").value
+    var entry = document.createElement('li')
+    entry.appendChild(document.createTextNode(comment))
+    list.appendChild(entry)
+
+}
